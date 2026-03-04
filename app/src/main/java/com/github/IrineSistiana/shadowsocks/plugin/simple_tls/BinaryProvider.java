abc275d3fddc1fb88770b3f97cb32ce250d1884a
onecreatorxx-arch <onecreatorxx@gmail.com>
2026-03-04 12:29:06 -0300

    minimal plugin base

---
+++ app/src/main/java/com/github/IrineSistiana/shadowsocks/plugin/simple_tls/BinaryProvider.java
@@ -0,0 +1,5 @@
+package com.github.IrineSistiana.shadowsocks.plugin.simple_tls;
+
+import com.github.shadowsocks.plugin.NativePluginProvider;
+
+public class BinaryProvider extends NativePluginProvider {}
