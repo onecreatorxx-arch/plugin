2f65b7735697fb0fa922aa178a87522d6d10db2e
onecreatorxx-arch <onecreatorxx@gmail.com>
2026-03-04 16:54:09 -0300

    Update main.go

--- transport/main.go
+++ transport/main.go
@@ -5,33 +5,32 @@ import (
 	"log"
 	"net"
 	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
-	// Ignorar todos los flags que Shadowsocks intente pasar
-	flag.CommandLine.Parse(os.Args[1:]) 
+
+	flag.CommandLine.Parse(os.Args[1:])
 
 	remote := os.Getenv("SS_REMOTE_HOST") + ":" + os.Getenv("SS_REMOTE_PORT")
 	local := net.JoinHostPort(os.Getenv("SS_LOCAL_HOST"), os.Getenv("SS_LOCAL_PORT"))
 
 	if os.Getenv("SS_LOCAL_HOST") == "" {
-		log.Println("Error: No se detectaron variables SIP003")
+		log.Println("Error: No SIP003 env detected")
 		os.Exit(1)
 	}
 
-	log.Printf("Plugin Activo | Local: %s | Remoto: %s", local, remote)
+	log.Printf("Plugin activo | Local: %s | Remoto: %s", local, remote)
 
-	// Crear el listener para que Shadowsocks detecte que el plugin "está listo"
 	l, err := net.Listen("tcp", local)
 	if err != nil {
-		log.Fatalf("Fallo al abrir puerto local: %v", err)
+		log.Fatal(err)
 	}
-	defer l.Close()
 
-	// Mantener el proceso vivo
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	<-sigCh
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			continue
+		}
+		conn.Close()
+	}
 }
